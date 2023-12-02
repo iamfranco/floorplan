28f4bb3761b481c2abb8ccd184ea17085aae7735
iamfranco <kinfaifrancochan@gmail.com>
2023-12-02 23:56:11 +0000

    Add basic room scene, with baked assets

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,13 +1,30 @@
-import { OrbitControls } from "@react-three/drei"
+import { OrbitControls, PerspectiveCamera, useGLTF, useTexture } from "@react-three/drei"
+import { GLTF } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+type GLTFResult = GLTF & {
+  nodes: {
+    baked: THREE.Mesh;
+  }
+}
 
 const Experience = () => {
+  const { nodes } = useGLTF('./models/scene.glb') as GLTFResult;
+  const bakedTexture = useTexture('./models/baked.jpg');
+  bakedTexture.flipY = false;
+
   return (
     <>
+      <PerspectiveCamera 
+        makeDefault
+        position={[-2, 6, 9]}
+      />
+
       <OrbitControls makeDefault />
 
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+      <mesh
+        geometry={nodes.baked.geometry}
+      >
+        <meshBasicMaterial map={bakedTexture} />
       </mesh>
     </>
   )
